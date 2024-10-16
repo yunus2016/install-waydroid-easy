71f3beecae8c6ff170887c0a4a4296c737c18154
yunus2016 <nykeskin@icloud.com>
2024-10-16 19:17:37 -0400

    Add files via upload
    so go into terminal and go sh install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt install curl ca-certificates -y
+curl -s https://repo.waydro.id | sudo bash
+sudo apt install waydroid -y
+sudo waydroid container start
+waydroid session start
+sudo apt install weston
